f0a6f85f9351eede46381377f4ca254731ad6d3d
Furkan BAYRAKTAR <me@furkanbayraktar.com>
2015-01-30 14:05:58 +0200

    Comments added to demo project.

--- test/src/main/java/com/valensas/vlsync/test/MainActivity.java
+++ test/src/main/java/com/valensas/vlsync/test/MainActivity.java
@@ -18,30 +18,56 @@ import com.valensas.vlsync.lib.VLSyncError;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateListener {
 
+    /**
+     * Debug tag.
+     */
     private static final String TAG = "VLSyncTest";
 
+    /**
+     * VLSync instance.
+     */
     private VLSync vlSync;
 
+    /**
+     * TextView used to display title.
+     */
     private TextView title;
-    private TextView description;
-    private ImageView image1;
 
+    /**
+     * TextView used to display description.
+     */
+    private TextView description;
+
+    /**
+     * ImageView used to display image.
+     */
+    private ImageView image;
+
+    /**
+     * Single selection dialog used to change VLSync options.
+     */
     private AlertDialog selectOptionsDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // UI initializations.
+
         setContentView(R.layout.activity_main);
 
         title = (TextView) findViewById(R.id.title);
         description = (TextView) findViewById(R.id.description);
-        image1 = (ImageView) findViewById(R.id.image1);
+        image = (ImageView) findViewById(R.id.image1);
+
+        // VLSync SDK initialization method call.
 
         initializeVLSync();
     }
@@ -49,13 +75,21 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
     @Override
     protected void onResume() {
         super.onResume();
-        vlSync.update(this);
+
+        // Get current time
+        Date dt = new Date();
+
+        // Check last update time. Every 30 minutes update
+        // if needed. It is not necessary because if there is
+        // no change, it will not download anything.
+        if(dt.getTime() - vlSync.lastUpdate() >= 1800000) {
+            vlSync.update(this);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
-
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -69,6 +103,9 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
             selectOptionsDialog.show();
             return true;
         }else if(item.getItemId() == R.id.update){
+            // Start update operation. If you do not want to
+            // display process indicator (aka HUD), you do not
+            // need to pass context to update method.
             vlSync.update(this);
             return true;
         }
@@ -76,6 +113,11 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * This method used to create an options dialog to
+     * demonstrate different types of options can be
+     * applied to VLSync.
+     */
     private void createOptionsDialog(){
         final String[] options = {"HUD Visible","HUD Hidden","Determinate Progress", "Indeterminate Progress", "Progress Text"};
 
@@ -83,8 +125,11 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
         optionBuilder.setTitle("Select option")
                 .setItems(options, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        // The 'which' argument contains the index position
-                        // of the selected item
+
+                        // Create a map object to store options. Fill the map according to
+                        // selected option. Options are not necessary, but useful for many cases.
+                        // You can also create your own flow by using callback methods provided
+                        // by OnUpdateListener.
                         Map<VLSync.UpdateOptionKey, VLSync.UpdateOptionValue> options = new HashMap<VLSync.UpdateOptionKey, VLSync.UpdateOptionValue>();
                         switch (which){
                             case 0:
@@ -106,7 +151,14 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
                                 options.put(VLSync.UpdateOptionKey.PROGRESS_STYLE, VLSync.ProgressStyle.DETERMINATE_TEXT_VISIBLE);
                                 break;
                         }
+                        // Set options. When the options are set, they become default
+                        // options for update operations. You can also set temporary
+                        // options by calling update method with options map.
                         vlSync.setUpdateOptions(options);
+
+                        // Start update operation. If you do not want to
+                        // display process indicator (aka HUD), you do not
+                        // need to pass context to update method.
                         vlSync.update(MainActivity.this);
                     }
                 });
@@ -114,39 +166,71 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
         selectOptionsDialog = optionBuilder.create();
     }
 
+    /**
+     * Initialization method for VLSync SDK. SDK must be initialized
+     * with project id and application context. Project Id can be
+     * obtained from VLSync Portal under the Settings menu.
+     */
     private void initializeVLSync(){
+        // Initialize SDK for usage. If you want to use SDK in any
+        // other places in your project, you do not need to initialize
+        // SDK. You can just use getInstance method of VLSync class
+        // to use it.
         vlSync = VLSync.initWithProjectId("df22a3aa-24ef-48fc-a4a6-0bcbdf70943e", getApplicationContext());
+
+        // If debugging set enabled, every step inside the SDK
+        // will be printed to console. It is useful to debug
+        // your project but recommended to be disabled when you
+        // release your application.
         vlSync.setDebugEnabled(true);
+
+        // Set on update listener to receive callbacks from SDK.
+        // It is not necessary to be set.
         vlSync.setOnUpdateListener(this);
     }
 
+    /**
+     * Refresh view to display updated data. Called after
+     * update operation. In demo project, we have a JSON file on
+     * VLSync server which contains 3 fields, title, description,
+     * and image. In this method, we first parse JSON to object and
+     * fill UI according to data.
+     */
     private void refreshView(){
         try {
+            // Parse JSON
             File file = new File(vlSync.getRootFolder(), "main.json");
             Gson gson = new Gson();
             BufferedReader br = new BufferedReader(new FileReader(file));
             MyObject object = gson.fromJson(br, MyObject.class);
 
+            // Set title and description
             title.setText(object.title);
             description.setText(object.description);
 
+            // Get image from storage and display.
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inPreferredConfig = Bitmap.Config.ARGB_8888;
             Bitmap bitmap = BitmapFactory.decodeFile(new File(vlSync.getRootFolder(), object.image).getAbsolutePath(), options);
-            image1.setImageBitmap(bitmap);
+            image.setImageBitmap(bitmap);
 
         }catch (Exception ex){
+            // If an error occurs, print error.
             ex.printStackTrace();
         }
     }
 
     @Override
     public void onPreUpdate() {
+        // Called just before the update operation.
         Log.d(TAG, "Update process is starting...");
     }
 
     @Override
     public void onPostUpdate(boolean success, VLSyncError error) {
+        // Called just after the update operation. If success is true,
+        // update operation is successful and error is null. Otherwise,
+        // error object contains a message about the failed operation.
         if(success) {
             refreshView();
         }else{
@@ -160,9 +244,16 @@ public class MainActivity extends ActionBarActivity implements VLSync.OnUpdateLi
 
     @Override
     public void onProgressUpdate(int progress) {
+        // When update progress changes, this method is called
+        // with new progress value.
         Log.d(TAG, "Progress received: " + progress);
     }
 
+    /**
+     * POJO for demo project. In demo project, we stored a JSON file
+     * in VLSync server called main.json. This class represents the
+     * object stored in json.
+     */
     private class MyObject {
         String title;
         String description;
